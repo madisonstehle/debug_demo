544c3e8009de50cd495428321f0c9cf78ae911e4
Roger Huba <rogehuba@amazon.com>
2019-11-22 23:45:31 -0800

    adding html and app.js

---
+++ app.js
@@ -0,0 +1,115 @@
+
+
+// #1
+
+// function raiseToPower(b, e) {
+//   return Math.pow(b, e);
+// }
+
+// var base = 2;
+// var exp = 3;
+// var power = raiseToPower(exp, base);
+// console.log(power);
+
+// #2
+
+// function getAnswer() {
+//   var x = 6;
+//   var y = 3;
+//   return x + y;
+// }
+
+// var result = getAnswers;
+// console.log(result);
+
+// #3
+
+// var myPay = 1000;
+// var myBills = 899;
+// var moneyLeftOver = recievables - payable;
+// console.log(`My leftover pay is: ${moneyLeftover}`);
+
+// #4
+
+// var innerHtml = "<p>Click here to <a href="#Home">return home</a></p>";
+// console.log(innerHtml);
+
+// #5
+
+// function count() {
+//   var oneToFive = "12345";
+//   var len = oneToFive.length;
+//   for (var i = 1; i <= len; i++) {
+//     console.log(oneToFive[i]);
+//   }
+// }
+
+// count();
+
+// #6
+
+// var myArray = [1, 2, 3;
+// for(var i = 0; i < myArray; i++){
+//     var arraySum += myArray[i];
+// }
+// console.log(`Total of array values are: ${arraySum}`);
+
+// #7
+
+// var x = 7;
+// var y = 9;
+// var result = "to come";
+
+// if(x = y) {
+//   result = "Equal!";
+// } else {
+//   result = "Not equal!";
+// }
+
+// console.log(result);
+
+// #8 
+
+// function myFunction() {
+//   for (var i = 1; i != 4; i += 2) {
+//     console.log("Energizer Bunny.  Still going!");
+//   }
+// }
+
+// #9
+
+// function zeroArray(m, n) {
+//   var newArray = [];
+//   var row = [];
+//   for (var i = 0; i < m; i++) {
+//     for (var j = 0; j < n; j++) {
+//       row.push(0);
+//     }
+//     newArray.push(row);
+//   }
+//   return newArray;
+// }
+
+// var matrix = zeroArray(3, 2);
+// console.log(matrix);
+
+// #10
+
+// function createAnArray(){
+//     var newArray = {};
+//     for(i = 0; i <= 10; i++){
+//         push.newArray(i);
+//     }
+//     return createAnArray;
+// }
+
+// function addArray(array) {
+//     for(var i = 1; i < array; i+){ 
+//         var sum = 0;
+//         sum += i;
+//     }
+//     return sum;
+// }
+
+// var newArr = createAnArray();
+// var add = addArray(newArr);
